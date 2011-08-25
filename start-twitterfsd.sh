b6771808d40ebc034955576b85aa87ae8bbc5f15
Kazuyoshi Aizawa <admin2@whiteboard.ne.jp>
2011-08-25 12:04:33 +0900

    rewrite script to refrect twitter4j library change.

--- start-twitterfsd.sh
+++ start-twitterfsd.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-CLASSPATH=cmd/lib/twitter4j-core-2.2.1.jar:cmd/dist/twitterfsd.jar
+CLASSPATH=cmd/lib/twitter4j-core-2.2.4.jar:cmd/dist/twitterfsd.jar:cmd/lib/twitter4j-stream-2.2.4.jar
 #java -Djava.util.logging.config.file=log.prop -cp $CLASSPATH iumfs.twitterfsd 
 java -cp $CLASSPATH iumfs.twitterfsd 
