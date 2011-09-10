9f3aaf0db6bbe809ddc79aa3d5a7b928ea0b76ee
Kazuyoshi Aizawa <admin2@whiteboard.ne.jp>
2011-09-10 19:56:50 +0900

    Set /Users/ka78231/NetBeansProjects/iumfs-twitterfs to class path

--- start-twitterfsd.sh
+++ start-twitterfsd.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-CLASSPATH=cmd/lib/twitter4j-core-2.2.4.jar:cmd/dist/twitterfsd.jar:cmd/lib/twitter4j-stream-2.2.4.jar
+CLASSPATH=$PWD/cmd/lib/twitter4j-core-2.2.4.jar:$PWD/cmd/dist/twitterfsd.jar:$PWD/cmd/lib/twitter4j-stream-2.2.4.jar
 #java -Djava.util.logging.config.file=log.prop -cp $CLASSPATH iumfs.twitterfs.Main
 java -cp $CLASSPATH iumfs.twitterfs.Main
