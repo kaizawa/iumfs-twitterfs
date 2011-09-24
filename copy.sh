8953711f6eebc7fdfd1fcde585cd0f47b386ee09
Kazuyoshi Aizawa <admin2@whiteboard.ne.jp>
2011-09-24 10:28:49 +0900

    Revemod unnecessary files

--- copy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cp -r cmd/src ~/tools/source/iumfs-twitterfs/cmd
-cp start.sh ~/tools/source/iumfs-twitterfs/
-cp build.xml ~/tools/source/iumfs-twitterfs/
-cp -r cmd/lib ~/tools/source/iumfs-twitterfs/cmd
-
